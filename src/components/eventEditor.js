4027eaa4a2038983aeb4d5e947a01a03abab44b6
GreedoRen <greedo.it@gmail.com>
2019-10-20 23:40:55 +0300

    module4-1

--- src/components/eventEditor.js
+++ src/components/eventEditor.js
@@ -1,12 +1,36 @@
-export const eventEdit = ({typeIcon, city, startTime, endTime, price, options, description, pictures}) => {
-  return `
-  <li class="trip-events__item">
+import {createElement} from "../utils/index";
+
+export class EventEdit {
+  constructor(data) {
+    this._typeIcon = data.typeIcon;
+    this._city = data.city;
+    this._startTime = data.startTime;
+    this._endTime = data.endTime;
+    this._price = data.price;
+    this._options = data.options;
+    this._description = data.description;
+    this._pictures = data.pictures;
+    this._element = null;
+  }
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+  getTemplate() {
+    return `
+      <li class="trip-events__item">
     <form class="event  event--edit" action="#" method="post">
       <header class="event__header">
         <div class="event__type-wrapper">
           <label class="event__type  event__type-btn" for="event-type-toggle-1">
             <span class="visually-hidden">Choose event type</span>
-            <img class="event__type-icon" width="17" height="17" src="img/icons/${typeIcon}.png" alt="Event type icon">
+            <img class="event__type-icon" width="17" height="17" src="img/icons/${this._typeIcon}.png" alt="Event type icon">
           </label>
           <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
           <div class="event__type-list">
@@ -62,7 +86,7 @@ export const eventEdit = ({typeIcon, city, startTime, endTime, price, options, d
           <label class="event__label  event__type-output" for="event-destination-1">
             Sightseeing at
           </label>
-          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${city}" list="destination-list-1">
+          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${this._city}" list="destination-list-1">
           <datalist id="destination-list-1">
             <option value="Amsterdam"></option>
             <option value="Geneva"></option>
@@ -73,19 +97,19 @@ export const eventEdit = ({typeIcon, city, startTime, endTime, price, options, d
           <label class="visually-hidden" for="event-start-time-1">
             From
           </label>
-          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${new Date(startTime).toLocaleString()}">
+          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${new Date(this._startTime).toLocaleString()}">
           &mdash;
           <label class="visually-hidden" for="event-end-time-1">
             To
           </label>
-          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${new Date(endTime).toLocaleString()}">
+          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${new Date(this._endTime).toLocaleString()}">
         </div>
         <div class="event__field-group  event__field-group--price">
           <label class="event__label" for="event-price-1">
             <span class="visually-hidden">Price</span>
             &euro;
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${price}">
+          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${this._price}">
         </div>
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
         <button class="event__reset-btn" type="reset">Delete</button>
@@ -104,7 +128,7 @@ export const eventEdit = ({typeIcon, city, startTime, endTime, price, options, d
         <section class="event__section  event__section--offers">
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
           <div class="event__available-offers">
-            ${options.map((option) => `<div class="event__offer-selector">
+            ${this._options.map((option) => `<div class="event__offer-selector">
               <input class="event__offer-checkbox  visually-hidden" id="${option.id}" type="checkbox" name="event-offer-luggage" ${option.isChecked ? `checked` : ``}>
               <label class="event__offer-label" for="${option.id}">
                 <span class="event__offer-title">${option.title}</span>
@@ -116,15 +140,15 @@ export const eventEdit = ({typeIcon, city, startTime, endTime, price, options, d
         </section>
         <section class="event__section  event__section--destination">
           <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-          <p class="event__destination-description">${description}</p>
+          <p class="event__destination-description">${this._description}</p>
           <div class="event__photos-container">
             <div class="event__photos-tape">
-               ${pictures.map((picture) => `<img class="event__photo" src="${picture}" alt="Event photo">`).join(``)}
+               ${this._pictures.map((picture) => `<img class="event__photo" src="${picture}" alt="Event photo">`).join(``)}
             </div>
           </div>
         </section>
       </section>
     </form>
-  </li>
-`;
-};
+  </li>`;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,56 +1,70 @@
-import {info} from './components/info';
-import {menu} from './components/menu';
-import {filters} from './components/filter';
-import {sort} from './components/sort';
-import {eventList} from './components/eventList';
-import {event} from './components/event';
-import {eventEdit} from './components/eventEditor';
+import {Info} from './components/info';
+import {Menu} from './components/menu';
+import {Filters} from './components/filter';
+// import {Sort} from './components/sort';
+import {EventList} from './components/eventList';
+import {Event} from './components/event';
+import {EventEdit} from './components/eventEditor';
 import {cityEvent, dateEvent, events} from "./utils/event";
 import {dataMenu} from "./utils/menu";
 import {dataFilters} from "./utils/filters";
+import {render} from './utils/index';
 
 
-const render = (container, html, place = `beforeend`) => {
-  container.insertAdjacentHTML(place, html);
-};
-const renderEvents = (containers, template, data, place = `beforeend`) => {
-  containers.forEach((container) => {
-    data.forEach((i) => container.insertAdjacentHTML(place, template(i)));
+const renderEvents = (data) => {
+  tripEventsList.forEach((container) => {
+    data.forEach((onlyCardData) => {
+      const eventCard = new Event(onlyCardData);
+      const eventForm = new EventEdit(onlyCardData);
+
+      eventCard.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
+        container.replaceChild(eventForm.getElement(), eventCard.getElement());
+      });
+
+      eventForm.getElement().querySelector(`.event--edit`).addEventListener(`submit`, () => {
+        container.replaceChild(eventCard.getElement(), eventForm.getElement());
+      });
+
+      eventForm.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
+        container.replaceChild(eventCard.getElement(), eventForm.getElement());
+      });
+      render(container, eventCard.getElement());
+    });
   });
 };
-const renderMenu = (container, template, data, place = `afterend`) => {
-  container.insertAdjacentHTML(place, template(data));
+const renderMenu = (data) => {
+  const tripControls = new Menu(data);
+  render(tripControlsElement, tripControls.getElement());
 };
-const renderFilers = (container, template, data, place = `afterend`) => {
-  container.insertAdjacentHTML(place, template(data));
+const renderFilers = (data) => {
+  const tripFilters = new Filters(data);
+  render(tripControlsElement, tripFilters.getElement());
 };
-const renderEventList = (container, template, data, place = `beforeend`) => {
-  container.insertAdjacentHTML(place, template(data));
+const renderEventList = (data) => {
+  const eventList = new EventList(data);
+  render(tripEventsContainer, eventList.getElement());
 };
-const renderTripInfo = (container, template, data, place = `afterbegin`) => {
-  container.insertAdjacentHTML(place, template(data));
+const renderTripInfo = (data) => {
+  const tripInfo = new Info(data);
+  render(tripInfoSection, tripInfo.getElement(), `begin`);
 };
-const renderEventEdit = (container, template, data, place = `afterbegin`) => {
-  container.insertAdjacentHTML(place, template(data));
+const updatePrice = (data) => {
+  costValueElement.innerHTML = data;
 };
-const pushCostSumm = (element, data) => {
-  element.innerHTML = data.reduce((value, currentItem) => {
-    return value + currentItem.price;
-  }, 0);
-};
+
 
 const tripInfoSection = document.querySelector(`section.trip-main__trip-info`);
-const tripControlsHeadings = document.querySelectorAll(`.trip-main__trip-controls h2`);
-const tripEventsSection = document.querySelector(`section.trip-events`);
+const tripControlsElement = document.querySelector(`.trip-controls`);
 const costValueElement = document.querySelector(`.trip-info__cost-value`);
-const eventListTempContainer = document.createElement(`div`);
+const tripEventsContainer = document.querySelector(`.trip-events`);
+let totalPrice = events.reduce((eventsPrice, currentItem) => {
+  return eventsPrice + currentItem.price;
+}, 0);
 
-renderTripInfo(tripInfoSection, info, cityEvent);
-renderMenu(tripControlsHeadings[0], menu, dataMenu); //
-renderFilers(tripControlsHeadings[1], filters, dataFilters); //
-renderEventList(eventListTempContainer, eventList, dateEvent);
-const tripEventsList = eventListTempContainer.querySelectorAll(`.trip-events__list`);
-renderEventEdit(tripEventsList[0], eventEdit, events[0]);
-renderEvents(tripEventsList, event, events);
-render(tripEventsSection, sort() + eventListTempContainer.innerHTML);
-pushCostSumm(costValueElement, events);
+renderTripInfo(cityEvent);
+renderMenu(dataMenu);
+renderFilers(dataFilters);
+renderEventList(dateEvent);
+const tripEventsList = tripEventsContainer.querySelectorAll(`.trip-events__list`);
+renderEvents(events);
+updatePrice(totalPrice);
